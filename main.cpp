9d88c6c226024cbd5245740b0c7eda064a975c33
Aris Dwi Maulana <dwiaris3765@gmail.com>
2026-05-20 10:52:16 +0700

    membuat alur dari menu secara simple

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cctype>
 using namespace std;
 
 
@@ -15,332 +14,200 @@ struct Transaksi{
     int tanggal[3];     
 };
 
-struct ViewData{
-    const int space[7] = {3, 12, 15, 6, 2, 2, 4};
-    const string metadata[7] = {
-        "id", "jenis", "deskripsi", "nominal", 
-        "tanggal", "bulan", "tahun"
-    };
-    Transaksi records[total_data];
-};
-
 //================================== Global Var Region
 Transaksi data_catatan[total_data];
-ViewData view_data;
 
-string last_page;
-
-//================================== View Utils Region
-string _request_choice_view(){
-    string pilihan;
-    cout << "--------------------" << endl; 
-    cout << "Pilihan: ";
-    cin >> pilihan;
+//================================== Function Region
+void list_menu(){
+    cout<<"1. Tampilkan Riwayat"<<endl;
+    cout<<"2. Catat Transaksi Baru"<<endl;
+    cout<<"3. Cari Transaksi"<<endl;
+    cout<<"4. Laporan Keuangan"<<endl;
+    cout<<"5. Edit Transaksi"<<endl;
+    cout<<"6. Hapus Transaksi"<<endl;
+    cout<<"7. Keluar"<<endl;
 }
 
-string _request_data_view(){
-    string data;
-    cout << "--------------------" << endl; 
-    cout << "Input : ";
-    cin >> data;
-}
-
-void _view_data_to_table(){
-    //gunakan struct view data untuk diconvert output bentuk tabel
-    //space untuk jarak antar column
-}
-
-//================================== Model Region
-bool isdigit_validation(const string &str){
-    if (str.empty()) return false;
-
-    for (char const &c : str) {
-        if (isdigit(c) == 0) return false;
-    }
-}
-
-bool validation_menu(const string &str, int n, int m) {
-    if (!isdigit_validation(str)){return false;}
-
-    int data = stoi(str);
-    if (n < data and m > data){
-        return true;
-    }
-    
-    return false;
-}
-
-bool new_record_validation(string data[]){
-    for (int i=2; i<6; i++){
-        if (!isdigit_validation(data[i])){
-            return false;
+void sub_menu_1(){
+    int sub_pilihan;
+    char sorted;
+    do{
+        system("cls");
+        cout<<"Sub Menu Tampilkan Riwayat"<<endl;
+        cout<<"1. Tampilkan Semua Transaksi"<<endl;
+        cout<<"2. Tampilkan Pemasukan"<<endl;
+        cout<<"3. Tampilkan Pengeluaran"<<endl;
+        cout<<"4. Kembali ke Menu Utama"<<endl;
+        cout<<"Pilihan: ";
+        cin>>sub_pilihan;
+        if(sub_pilihan >= 1 && sub_pilihan <= 3){
+            cout<<"Urutkan ? (y/n): ";
+            cin>>sorted;
         }
-    }
-
-    int tanggal = stoi(data[3]);
-    int bulan = stoi(data[4]);
-    int tahun = stoi(data[5]);
-    
-    if (tanggal <= 0 and tanggal > 31){return false;}
-    if (bulan <= 0 and bulan > 12){return false;}
-    if (tahun < 2020){return false;}
-
-    return true;
-}
-
-void search_record(const string &str){
-    int taget;
-    //if (str == "1"){taget = }
-}
-
-void sort_record(const string &str){
-    //pilih sort
-}
-
-//================================== View Region
-string home_view(){
-    cout << "\n=== PROGRAM MANAGEMENT KEUANGAN ===\n";
-    cout << "1. Tampilkan Riwayat\n";
-    cout << "2. Catat Transaksi Baru\n";
-    cout << "3. Cari Transaksi\n";
-    cout << "4. Laporan Keuangan\n";
-    cout << "5. Edit Transaksi\n";
-    cout << "6. Hapus Transaksi\n";
-    cout << "7. Keluar\n";
-
-    return _request_choice_view();
-}
-
-string riwayat_menu_view(){
-    cout << "1. Semua Transaksi" << endl
-         << "2. Pemasukan" << endl 
-         << "3. Pengeluaran" << endl;
-
-    return _request_choice_view();
-}
-
-string laporan_menu_view(){
-    cout << "1. Tahun" << endl
-         << "2. Bulan" << endl
-         << "3. Semua" << endl;
-
-    return _request_choice_view();
-}
-
-string laporan_request_view(string option){
-    string tahun, bulan, list[2];
-    if (option != "3"){
-        cout << "Masukkan Tahun : ";
-        cin >> tahun;
-        if (option == "1"){
-            list[0] = tahun;
-            return "";
+        switch(sub_pilihan){
+            case 1:
+                system("cls");
+                sorted == 'y' || sorted == 'Y' ? cout<<"(Terurut)..."<<endl : cout<<"(Tidak Terurut)..."<<endl;
+                system("pause");
+                break;
+            case 2:
+                system("cls");
+                sorted == 'y' || sorted == 'Y' ? cout<<"(Terurut)..."<<endl : cout<<"(Tidak Terurut)..."<<endl;
+                cout<<"Menampilkan Pemasukan..."<<endl;
+                system("pause");
+                break;
+            case 3:
+                system("cls");
+                sorted == 'y' || sorted == 'Y' ? cout<<"(Terurut)..."<<endl : cout<<"(Tidak Terurut)..."<<endl;
+                cout<<"Menampilkan Pengeluaran..."<<endl;
+                system("pause");
+                break;
+            case 4:
+                system("cls");
+                cout<<"Kembali ke Menu Utama..."<<endl;
+                break;
+            default:
+                system("cls");
+                cout<<"Pilihan tidak valid. Silakan coba lagi."<<endl;
+                system("pause");
         }
-        cout << "Masukkan Bulan : ";
-        cin >> bulan;
-        if (option == "2"){
-            list[0] = tahun;
-            list[1] = bulan;
-            return "";
+    }while(sub_pilihan != 4);
+}
+
+void sub_menu_3(){
+    int sub_pilihan;
+    do{
+        system("cls");
+        cout<<"Sub Menu Cari Transaksi"<<endl;
+        cout<<"1. Cari berdasarkan 1 Tahun"<<endl;
+        cout<<"2. Cari berdasarkan 1 Bulan"<<endl;
+        cout<<"3. Cari berdasarkan 1 Hari"<<endl;
+        cout<<"4. Cari berdasarkan nominal"<<endl;
+        cout<<"5. Kembali ke Menu Utama"<<endl;
+        cout<<"Pilihan: ";
+        cin>>sub_pilihan;
+        switch(sub_pilihan){
+            case 1:
+                system("cls");
+                cout<<"Mencari Transaksi berdasarkan 1 Tahun..."<<endl;
+                system("pause");
+                break;
+            case 2:
+                system("cls");
+                cout<<"Mencari Transaksi berdasarkan 1 Bulan..."<<endl;
+                system("pause");
+                break;
+            case 3:
+                system("cls");
+                cout<<"Mencari Transaksi berdasarkan 1 Hari..."<<endl;
+                system("pause");
+                break;
+            case 4:
+                system("cls");
+                cout<<"Mencari Transaksi berdasarkan nominal..."<<endl;
+                system("pause");
+                break;
+            case 5:
+                system("cls");
+                cout<<"Kembali ke Menu Utama..."<<endl;
+                break;
+            default:
+                system("cls");
+                cout<<"Pilihan tidak valid. Silakan coba lagi."<<endl;
+                system("pause");
         }
-    }
+    }while(sub_pilihan != 5);
 }
 
-string sort_menu_view(){
-    cout << "1. Ascending" << endl
-         << "2. Descending" << endl
-         << "3. Asli" << endl;
-
-    return _request_choice_view();
+void sub_menu_4(){
+    int sub_pilihan;
+    do{
+        system("cls");
+        cout<<"Sub Menu Laporan Keuangan"<<endl;
+        cout<<"1. Laporan Bulanan"<<endl;
+        cout<<"2. Laporan Tahunan"<<endl;
+        cout<<"3. Laporan All Time"<<endl;
+        cout<<"4. Kembali ke Menu Utama"<<endl;
+        cout<<"Pilihan: ";
+        cin>>sub_pilihan;
+        switch(sub_pilihan){
+            case 1:
+                system("cls");
+                cout<<"Menampilkan Laporan Bulanan..."<<endl;
+                system("pause");
+                break;
+            case 2:
+                system("cls");
+                cout<<"Menampilkan Laporan Tahunan..."<<endl;
+                system("pause");
+                break;
+            case 3:
+                system("cls");
+                cout<<"Menampilkan Laporan All Time..."<<endl;
+                system("pause");
+                break;
+            case 4:
+                system("cls");
+                cout<<"Kembali ke Menu Utama..."<<endl;
+                break;
+            default:
+                system("cls");
+                cout<<"Pilihan tidak valid. Silakan coba lagi."<<endl;
+                system("pause");
+        }
+    }while(sub_pilihan != 4);
 }
-
-string search_menu_view(){
-    cout << "1. Tahun" << endl
-         << "2. Bulan" << endl
-         << "3. Hari" << endl
-         << "4. Nominal" << endl;
-
-    return _request_choice_view(); 
-}
-
-string add_record_view(string container[]){
-    string jenis, deskripsi, nominal, tanggal[3];
-
-    cout << "Id : " << "kosong" << endl;
-
-    cout << "Jenis (1: Pemasukan, 2: Pengeluaran): ";
-    cin >> container[0];
-
-    cout << "Deskripsi : ";
-    cin >> container[1];
-
-    cout << "Nominal : ";
-    cin >> container[2];
-
-    cout << "Tanggal : ";
-    cin >> container[3];
-
-    cout << "Bulan : ";
-    cin >> container[4];
-
-    cout << "Tahun : ";
-    cin >> container[5];
-}
-
-string edit_record_view(){
-    string id;
-    cout << "Masukkan ID yang akan dihapus: "; 
-    cin >> id;
-
-    return id;
-}
-
-string delete_record_view(){
-    string id;
-    cout << "Masukkan ID yang akan dihapus: "; 
-    cin >> id;
-
-    return id;
-}
-
-string error_view(){
-    cout << "--------------------" << endl; 
-    cout << "Masukkan Input Dengan Benar !!!";
-    cout << "--------------------" << endl;
-
-    return last_page;
-}
-
-void show_transaksi_view(){
-
-}
-
-
-//================================== Gateway Region
-void add_record(){
-
-}
-
-void delete_record(){
-
-}
-
-void update_record(){
-
-}
-
-//================================== Mapper Region
-
-//Opsional
-
-//================================== Controller Region
-string home_page_handler(){
-    string request;
-    string menu[] = {
-        "ERROR", "RIWAYAT", "CATAT", "CARI",  
-        "LAPORAN", "EDIT", "HAPUS", "KELUAR"
-    };
-
-    request = home_view();
-    if (validation_menu(request, 1, 7)){
-        return menu[stoi(request)];
-    }else{
-        last_page = "HOME";
-        return menu[0];
-    }
-}
-
-string riwayat_page_handler(){
-    string request;
-    string menu[] = {
-        "ERROR", "SEMUA_CATATAN", 
-        "PENGELUARAN", "PEMASUKAN"
-    };
-    
-    request = riwayat_menu_view();
-    if (validation_menu(request, 1, 3)){
-        return menu[stoi(request)];
-    }else{
-        last_page = "RIWAYAT";
-        return menu[0];
-    }
-}
-
-string laporan_page_handler(){
-    string request1, request2;
-    request1 = laporan_menu_view();
-
-    if (validation_menu(request1, 1, 3)){
-        //lanjutan
-    }else{
-        last_page = "LAPORAN";
-        return "ERROR";
-    }
-}
-
-string catat_page_handler(){
-    string request[6];
-    add_record_view(request);
-
-    if (new_record_validation(request)){
-        add_record();
-    } else {
-        last_page = "CATAT";
-        return "ERROR"; 
-    }
-}
-
-string search_page_handler(){
-    string request1, request2;
-    request1 = search_menu_view();
-
-    if (validation_menu(request1, 1, 4)){
-        request2 = _request_data_view();
-        //validator
-
-    } else {
-        last_page = "SEARCH";
-        return "ERROR";
-    }
-}
-
-string sort_page_handler(){
-    string request;
-    request = search_menu_view();
-    if (validation_menu(request, 1, 3)){
-        //eksekusi sort
-    } else {
-        last_page = "SORT";
-        return "ERROR";
-    }
-}
-
 //================================== main Region
 int main() {
-    string halaman = "HOME";
-
-    while (halaman != "KELUAR"){
-
-        if (halaman == "HOME"){
-            halaman = home_page_handler();
+    int pilihan;
+    do{
+        system("cls");
+        cout<<"========== Program Catatan Keuangan =========="<<endl;
+        list_menu();
+        cout<<"--------------------"<<endl;
+        cout<<"Pilihan: ";
+        cin>>pilihan;
+        switch(pilihan){
+            case 1:
+                system("cls");
+                sub_menu_1();
+                system("pause");
+                break;
+            case 2:
+                system("cls");
+                cout<<"Mencatat Transaksi Baru..."<<endl;
+                system("pause");
+                break;
+            case 3:
+                system("cls");
+                sub_menu_3();
+                system("pause");
+                break;
+            case 4:
+                system("cls");
+                sub_menu_4();
+                system("pause");
+                break;
+            case 5:
+                system("cls");
+                cout<<"Mengedit Transaksi..."<<endl;
+                system("pause");
+                break;
+            case 6:
+                system("cls");
+                cout<<"Menghapus Transaksi..."<<endl;
+                system("pause");
+                break;
+            case 7:
+                system("cls");
+                cout<<"Keluar dari Program. Terima kasih!"<<endl;
+                break;
+            default:
+                system("cls");
+                cout<<"Pilihan tidak valid. Silakan coba lagi."<<endl;
+                system("pause");
         }
-
-        else if (halaman == "RIWAYAT"){
-            halaman = riwayat_page_handler();
-        }
-
-        else if (halaman == "LAPORAN"){
-            halaman = riwayat_page_handler();
-        }
-
-        else if (halaman == "CATAT"){
-            halaman = catat_page_handler();
-        }
-
-        else if (halaman == "ERROR"){
-            halaman = error_view();
-        }
-    }
+    }while(pilihan != 7);
 
     return 0;
 }
